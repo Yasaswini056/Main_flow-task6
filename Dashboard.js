d73e78c2323ddee8e19dc1136074da4a64077d1c
Yasaswini056 <yasaswini.n06@gmail.com>
2024-12-25 21:02:52 +0530

    Add files via upload

---
+++ Dashboard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles.css';
+
+const Dashboard = () => {
+    return (
+        <div className="dashboard">
+            <h1>Welcome to the LearnToCode</h1>
+            <p>Start your coding journey with easy tutorials and resources
+            </p>
+            
+        </div>
+    );
+};
+
+export default Dashboard;
+
